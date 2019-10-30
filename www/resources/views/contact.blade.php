ced3a1babef0e8116ba414ca41cc6c8a7f6c7826
cedricbd <cebuisso@gmail.com>
2019-10-30 12:03:29 +0100

    mise a jour contact

--- www/resources/views/contact.blade.php
+++ www/resources/views/contact.blade.php
@@ -7,9 +7,71 @@ Contact
 @endsection
 
 @section('content')
+<section class="mb-4">
 
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores incidunt saepe suscipit, voluptatum nesciunt at sed ut quis. Est laborum perspiciatis dolores nemo deserunt libero voluptates magnam illum soluta repellat.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi nihil sunt eius natus itaque, quis totam doloremque consectetur vero? Possimus ipsa dolore itaque sit quo. Distinctio totam fugiat voluptatem eius?</p>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus, at, nulla aliquid voluptas eligendi obcaecati, alias quae voluptatum praesentium consectetur ullam inventore. Nemo explicabo temporibus consequatur voluptates sed deserunt reprehenderit.lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo iusto distinctio autem ipsa voluptate veniam quis, fuga id laborum itaque praesentium similique omnis dignissimos ut neque, obcaecati quia tempore error?</p>
+        <!--Grid column-->
+    
+            <form id="contact-form" name="contact-form" action="mail.php" method="POST">
+
+                <!--Grid row-->
+                <div class="row">
+
+                    <!--Grid column-->
+                    <div class="col-md-6">
+                        <div class="md-form mb-0">
+                            <input type="text" id="name" name="name" class="form-control" placeholder="Your name">
+                            <label for="name" class=""></label>
+                        </div>
+                    </div>
+                    <!--Grid column-->
+
+                    <!--Grid column-->
+                    <div class="col-md-6">
+                        <div class="md-form mb-0">
+                            <input type="text" id="email" name="email" class="form-control" placeholder="Your email">
+                            <label for="email" class=""></label>
+                        </div>
+                    </div>
+                    <!--Grid column-->
+
+                </div>
+                <!--Grid row-->
+
+                <!--Grid row-->
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="md-form mb-0">
+                            <input type="text" id="subject" name="subject" class="form-control" placeholder="Subject">
+                            <label for="subject" class=""></label>
+                        </div>
+                    </div>
+                </div>
+                <!--Grid row-->
+
+                <!--Grid row-->
+                <div class="row">
+
+                    <!--Grid column-->
+                    <div class="col-md-12">
+
+                        <div class="md-form">
+                            <textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea" placeholder="Your message"></textarea>
+                            <label for="message"></label>
+                        </div>
+
+                    </div>
+                </div>
+                <!--Grid row-->
+
+            </form>
+
+            <div class="text-center text-md-left">
+                <a class="btn btn-primary" onclick="document.getElementById('contact-form').submit();">Send</a>
+            </div>
+            <div class="status"></div>
+        
+        <!--Grid column-->
+
+</section>
     
 @endsection
--- www/resources/views/partials/index.blade.php
+++ www/resources/views/partials/index.blade.php
@@ -9,8 +9,6 @@
     <title>@yield('titlePage')</title>
 </head>
 <body>
-
-
 <nav id="navbar" class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="/">Home</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -18,8 +16,8 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
           <ul class="navbar-nav">
-            <li class="nav-item active">
-              <a class="nav-link" href="/blog">blog<span class="sr-only">(current)</span></a>
+            <li class="nav-item">
+              <a class="nav-link" href="/blog">blog</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="/about">about</a>
