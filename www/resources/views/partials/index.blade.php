49a483cd84e15c86986811050332e486435de3c4
cedricbd <cebuisso@gmail.com>
2019-10-30 14:48:56 +0100

    mise a jour index avecsection col1 et col2

--- www/resources/views/partials/index.blade.php
+++ www/resources/views/partials/index.blade.php
@@ -8,8 +8,10 @@
     
     <h1 class="text-center py-4">@yield('titleContent')</h1>
 
-    <div id="section" class="container pt-4">    
-        @yield('content')
+    <div id="section" class="container pt-4">
+        <div class="row"> 
+            <div class="col-lg-6 col-sm-12">@yield('col1')</div>
+            <div class="col-lg-6 col-sm-12">@yield('col1')</div> 
     </div>
 </div>
 @include('partials/footer')
